c274a031adeefc8a51e1d3345286d2d62bf18236
Matias Mortara <m.mortara01@gmail.com>
2024-09-15 00:13:43 -0300

    fix: webpack configs

--- dist/ThemeSwitcher.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { FaSun, FaMoon } from 'react-icons/fa';
-import { useTheme } from './ThemeContext';
-import './App.css';
-var ThemeSwitcher = function () {
-    var _a = useTheme(), theme = _a.theme, toggleTheme = _a.toggleTheme;
-    return (_jsx("button", { onClick: toggleTheme, className: "theme-switcher", children: theme === 'light' ? _jsx(FaMoon, {}) : _jsx(FaSun, {}) }));
-};
-export default ThemeSwitcher;
